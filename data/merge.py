0837806c9a1efbabe70530288b5bd8cac0f22adf
ZonggenLi <zonggenli@gmail.com>
2025-01-14 14:11:08 +0100

    added merged csv file for events and fatalities

---
+++ data/merge.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+events_file = "data/number_of_political_violence_events_by_country-year_as-of-13Dec2024.csv"
+fatalities_file = "data/number_of_reported_fatalities_by_country-year_as-of-13Dec2024.csv"
+
+events_data = pd.read_csv(events_file, sep=';')
+fatalities_data = pd.read_csv(fatalities_file, sep=';')
+
+merged_data = pd.merge(events_data, fatalities_data, on=['Country', 'Year'], how='outer')
+
+output_file = "data/merged_country_year_events_fatalities.csv"
+merged_data.to_csv(output_file, sep=';', index=False)
